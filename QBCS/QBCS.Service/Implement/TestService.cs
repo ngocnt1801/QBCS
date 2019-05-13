9baf1782e57c2832e94a597b31234cb63b2abb79
ngocnt1801 <ngocnt1801@gmail.com>
2019-05-13 18:57:58 +0700

    NgocNT_Sprint1_Add elmah log + TestService for example

---
+++ QBCS/QBCS.Service/Implement/TestService.cs
@@ -0,0 +1,20 @@
+﻿using QBCS.Repository.Implement;
+using QBCS.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QBCS.Service.Implement
+{
+    class TestService
+    {
+        private IUnitOfWork u;
+        public TestService()
+        {
+            u = new UnitOfWork();
+        }
+
+    }
+}
