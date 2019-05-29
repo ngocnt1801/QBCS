1a5c10e780ff0d062c9cbdcafe2b254a08ebb2c1
truongnbse62373 <truongnbse62373@fpt.edu.vn>
2019-05-30 04:28:40 +0700

    TruongNB_Sprint3_Update Rule UI not functional yet

---
+++ QBCS/QBCS.Service/Implement/RuleService.cs
@@ -0,0 +1,33 @@
+﻿using QBCS.Repository.Implement;
+using QBCS.Repository.Interface;
+using QBCS.Service.Interface;
+using QBCS.Service.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QBCS.Entity;
+
+namespace QBCS.Service.Implement
+{
+    public class RuleService: IRuleService
+    {
+        private IUnitOfWork unitOfWork;
+
+        public RuleService()
+        {
+            unitOfWork = new UnitOfWork();
+        }
+        public List<RuleViewModel> getAllRule()
+        {
+            List<RuleViewModel> result = new List<RuleViewModel>();
+            //List<RuleKey> rules = unitOfWork.Repository<>
+            return result;
+        }
+        public bool UpdateRule(RuleViewModel rule)
+        {
+            return false;
+        }
+    }
+}
---
+++ QBCS/QBCS.Web/Controllers/RuleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QBCS.Web.Controllers
+{
+    public class RuleController : Controller
+    {
+        // GET: Rule
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
