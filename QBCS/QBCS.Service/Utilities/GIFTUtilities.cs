d1cd85ea2c13fe1887f032b954098c1ccc19e8b8
ngocnt1801 <ngocnt1801@gmail.com>
2019-06-24 13:40:16 +0700

    NhiNPT_Sprint6_read mc

--- QBCS/QBCS.Service/Utilities/GIFTUtilities.cs
+++ QBCS/QBCS.Service/Utilities/GIFTUtilities.cs
@@ -215,32 +215,32 @@ namespace QBCS.Service.Utilities
                             }
                             
                         }
-                        //if (mark != null)
-                        //{
-                        //    if (!mark.Equals(""))
-                        //    {
-                        //        if (int.Parse(mark) < 0)
-                        //        {
-                        //            countWrong++;
-                        //            countRight = 0;
-                        //            isBlock = true;
-                        //            isMultipleChoice = false;
-                        //            mark = null;
-                        //            continue;
-                        //        }
-                        //        if (int.Parse(mark) > 0)
-                        //        {
-                        //            countRight++;
-                        //            countWrong = 0;
-                        //            isBlock = true;
-                        //            isMultipleChoice = false;
-                        //            mark = null;
-                        //            continue;
-                        //        }
-                        //    }
-                            
-                        //}
-                       
+                        if (mark != null)
+                        {
+                            if (!mark.Equals(""))
+                            {
+                                if (int.Parse(mark) < 0)
+                                {
+                                    countWrong++;
+                                    countRight = 0;
+                                    isBlock = true;
+                                    isMultipleChoice = false;
+                                    mark = null;
+                                    continue;
+                                }
+                                if (int.Parse(mark) > 0)
+                                {
+                                    countRight++;
+                                    countWrong = 0;
+                                    isBlock = true;
+                                    isMultipleChoice = false;
+                                    mark = null;
+                                    continue;
+                                }
+                            }
+
+                        }
+
                         if (countWrong >= 1 && !isEnd)
                         {
                             if (let == '%' && countElement == 1)
