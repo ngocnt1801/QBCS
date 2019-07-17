98e64c86ae5775e9d381d011ec4db3ad684a1849
ngocnt1801 <ngocnt1801@gmail.com>
2019-07-17 17:37:09 +0700

    NgocNT_Sprint9_add start sql in startup class

--- QBCS/QBCS.Web/Startup.cs
+++ QBCS/QBCS.Web/Startup.cs
@@ -8,8 +8,10 @@ namespace QBCS.Web
 {
     public class Startup
     {
+        string connectionString = ConfigurationManager.ConnectionStrings["QBCSContext"].ConnectionString;
         public void Configuration(IAppBuilder app)
         {
+            System.Data.SqlClient.SqlDependency.Start(connectionString);
             app.MapSignalR();
             app.UseStpmAuthentication(ConfigurationManager.AppSettings);
             app.UseStpmModuleManager(
