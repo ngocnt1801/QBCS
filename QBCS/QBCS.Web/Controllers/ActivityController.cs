7130a9eacb13a20d493a6b6e1b906d40c268bddc
truongnbse62373 <truongnbse62373@fpt.edu.vn>
2019-07-21 12:35:40 +0700

    TruongNB_Sprint9

--- QBCS/QBCS.Web/Controllers/ActivityController.cs
+++ QBCS/QBCS.Web/Controllers/ActivityController.cs
@@ -46,8 +46,8 @@ namespace QBCS.Web.Controllers
 
         //Staff
         //stpm: feature declare
-        //[Feature(FeatureType.SideBar, "Get All Activities", "QBCS", protectType: ProtectType.Authorized, ShortName = "All Activities", InternalId = (int)SideBarEnum.AllActivity)]
-        //[LogAction(Action = "Activities", Message = "View All Activities", Method = "GET")]
+        [Feature(FeatureType.SideBar, "Get All Activities", "QBCS", protectType: ProtectType.Authorized, ShortName = "All Activities", InternalId = (int)SideBarEnum.AllActivity)]
+        [LogAction(Action = "Activities", Message = "View All Activities", Method = "GET")]
         public ActionResult GetAllActivities()
         {
             //List<LogViewModel> logViews = new List<LogViewModel>();
