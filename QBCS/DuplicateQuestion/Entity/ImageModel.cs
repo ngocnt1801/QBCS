bdbcf7c0f3c0c378e0990915c34cb84d999e9068
ngocnt1801 <ngocnt1801@gmail.com>
2019-07-25 14:43:24 +0700

    NgocNT_Sprint9_Move image from question to questionTemp

---
+++ QBCS/DuplicateQuestion/Entity/ImageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateQuestion.Entity
+{
+    public class ImageModel
+    {
+        public int Id { get; set; }
+        public string Source { get; set; }
+        public int QuestionId { get; set; }
+        public int QuestionTempId { get; set; }
+    }
+}
