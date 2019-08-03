b6a8fc3d29b2f90c682d3a42e51e6014bf292643
ngocnt1801 <ngocnt1801@gmail.com>
2019-08-03 23:21:25 +0700

    NgocNT_Sprint10_remove js bank-result

--- QBCS/QBCS.Web/Scripts/myjs/bank-result.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    nav_bar_active();
-    hideAll();
-    loadFirstTable();
-    clickSection();
-    customs_display();
-    customs_display_duplicate();
-    table_on_top();
-});
