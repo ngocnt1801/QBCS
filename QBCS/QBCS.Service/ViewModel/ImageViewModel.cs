e3ec991fd19e801434467df349cc84d27bf0886f
Thanh Lâm <lamttse62614@fpt.edu.vn>
2019-08-08 23:04:35 +0700

    LamTT_Sprint12 export, create exam with multiple image

--- QBCS/QBCS.Service/ViewModel/ImageViewModel.cs
+++ QBCS/QBCS.Service/ViewModel/ImageViewModel.cs
@@ -17,5 +17,10 @@ namespace QBCS.Service.ViewModel
         public int? QuestionTempId { get; set; }
 
         public int? QuestionInExamId { get; set; }
+        public int? OptionId { get; set; }
+
+        public int? OptionTempId { get; set; }
+
+        public int? OptionInExamId { get; set; }
     }
 }
